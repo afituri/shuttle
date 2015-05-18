8b5f7c4ec958c870b106b004c99304424464c200
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-05-18 13:05:40 +0200

    start with dashbord layout

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/world', function(req, res) {
+  res.render('world', { title: 'Express' });
+});
+
 /* GET register page. */
 router.get('/register', function(req, res) {
   res.render('register', { title: 'Express' });
